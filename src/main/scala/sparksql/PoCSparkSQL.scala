807c3d89f6ba3a6754485c1d2da27a70046719f7
DIR\j.garcia.navarro <juanmanuel.gn@gmail.com>
2018-10-15 16:30:46 +0200

    Initial commit

---
+++ src/main/scala/sparksql/PoCSparkSQL.scala
@@ -0,0 +1,41 @@
+package sparksql
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+case class Partido(key:String,
+                   temporada:String,
+                   jornada:String,
+                   equipoCasa:String,
+                   equipoVisitante:String,
+                   golesCasa:String,
+                   golesVisitante:String,
+                   fecha:String,
+                   timestamp:String)
+class PoCSparkSQL {
+  val spark = SparkSession.builder
+    .master("local")
+    .appName("Word Count")
+    .config("spark.some.config.option", "some-value")
+    .getOrCreate()
+
+  val df = spark.read.textFile("data/DataSetPartidos.txt")
+  def partidosDF: RDD[Partido] =
+    spark.sparkContext
+    .textFile("data/DataSetPartidos.txt")
+    .map(_.split("::"))
+    .map(fields =>
+      Partido(
+        fields(0),fields(1),fields(2),
+        fields(3),fields(4),fields(5),
+        fields(6),fields(7),fields(8)))
+}
+object PoCSparkSQL extends App {
+  val poc = new PoCSparkSQL
+  poc.df.show()
+  val df = poc.spark.createDataFrame(poc.partidosDF)
+  df.createOrReplaceTempView("partidos")
+  val partidosMalaga = poc.spark.sql("select * from partidos where equipoCasa == 'CD Malaga'")
+  partidosMalaga.show()
+
+}
